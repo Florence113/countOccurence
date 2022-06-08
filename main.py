ecd7df772593f48c58c7d7bfedcd8b2f56c47649
Florence <72282730+Florence113@users.noreply.github.com>
2022-06-08 01:35:35 +0100

    New changes

--- main.py
+++ main.py
@@ -8,26 +8,18 @@ def read_file_content(filename):
 
     with open ("story.txt", "r") as f:
         lines = f.read().lower()
-        print (lines)
-#         split_lines = lines.split()
-#         print(split_lines)
-        
-#         count = {}
-#     for text in split_lines:
-#         if text in count:
-#             count[text] += 1
-#         else:
-#             count[text] = 1
-#     print (count)
 
-# read_file_content('story.txt')        
+    return lines
+
+print (read_file_content('story.txt'))       
 
 def count_words():
     word = read_file_content('story.txt')
     # # [assignment] Add your code here
-    print(word)
-    split_lines = text.split()
+
+    split_lines = word.split()
     print(split_lines)
+    
     count = {}
     for text in split_lines:
         if text in count:
@@ -36,5 +28,4 @@ def count_words():
             count[text] = 1
     return count   
 
-count_words()
-
+print(count_words())
